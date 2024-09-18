f5604c0cf2e566a616e686fdb1f31ca8f38dcd2f
QiuLiang-99 <1297829693@qq.com>
2024-09-18 19:36:55 +0800

    new feature

---
+++ plugins/livereminder/api.h
@@ -0,0 +1,8 @@
+#include <QUrl>
+
+namespace LR {
+  namespace api {
+    const QUrl get_huyaID_api =
+        QUrl("https://www.huya.com/udb_web/checkLogin.php", QUrl::StrictMode);
+  } // namespace api
+} // namespace LR
